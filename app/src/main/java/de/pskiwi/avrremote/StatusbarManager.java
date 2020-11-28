f3cee35bb0fa9b32cb955c40920fe3a87184e639
Andreas Pillath <andreas.pillath@gmail.com>
2020-11-28 14:28:14 +0100

    Enable static notification. Fixes #5

--- app/src/main/java/de/pskiwi/avrremote/StatusbarManager.java
+++ app/src/main/java/de/pskiwi/avrremote/StatusbarManager.java
@@ -17,17 +17,21 @@
 package de.pskiwi.avrremote;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public final class StatusbarManager {
 
 	public StatusbarManager(AVRApplication app) {
 		this.app = app;
 		notificationManager = (NotificationManager) app
-				.getSystemService(Context.NOTIFICATION_SERVICE);
+				.getSystemService(NOTIFICATION_SERVICE);
 
 		int icon = R.drawable.icon;
 		CharSequence tickerText = "AVR-Remote";
@@ -48,8 +52,34 @@ public final class StatusbarManager {
 		PendingIntent contentIntent = PendingIntent.getActivity(app, 0,
 				notificationIntent, 0);
 
-//		notification.setLatestEventInfo(context, contentTitle, contentText,
-//				contentIntent);
+        // https://stackoverflow.com/questions/32345768/cannot-resolve-method-setlatesteventinfo
+		// https://developer.android.com/guide/topics/ui/notifiers/notifications.html
+		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+			Notification.Builder builder = new Notification.Builder(context);
+
+			builder.setAutoCancel(false);
+			builder.setContentIntent(contentIntent);
+			builder.setContentTitle(contentTitle);
+			builder.setContentText(contentText);
+			builder.setSmallIcon(R.drawable.icon_small);
+			builder.setOngoing(true);
+
+			NotificationManager manager = (NotificationManager) app.getSystemService(NOTIFICATION_SERVICE);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+			{
+				String channelId = "avr_remote_channel";
+				NotificationChannel channel = new NotificationChannel(
+						channelId,
+						"AVR-Remote",
+						NotificationManager.IMPORTANCE_DEFAULT);
+				manager.createNotificationChannel(channel);
+				builder.setChannelId(channelId);
+			}
+
+			Notification notification=builder.build();
+
+			manager.notify(0,notification);
+		}
 	}
 
 	public void update() {
